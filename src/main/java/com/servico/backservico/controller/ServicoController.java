5a6aa0e34872b1b4a6af323a35e20b3b6aa7b3e1
alexandredodge <alexandredodge@hotmail.com>
2023-06-27 16:46:17 -0400

    Atualização

--- src/main/java/com/servico/backservico/controller/ServicoController.java
+++ src/main/java/com/servico/backservico/controller/ServicoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,13 +14,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.servico.backservico.entity.Servico;
 import com.servico.backservico.service.ServicoService;
 
 @RestController
 @RequestMapping("/api/servico")
+@CrossOrigin("http://localhost:3000")
 public class ServicoController {
 
 	//Não estou utilizando diretamente a classe do repository e sim a classe intermediaria de serviço "ServicoService"
@@ -27,6 +28,7 @@ public class ServicoController {
 	private ServicoService servicoService;
 	
 	@GetMapping("/")
+	
 	public List<Servico> buscarTodos(){
 		return servicoService.buscarTodos();
 	}
